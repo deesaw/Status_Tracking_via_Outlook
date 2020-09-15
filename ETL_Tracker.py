731ade266ad3dab8ae8ca81f5324b085df5768d4
Deepa Saw <69420960+deesaw@users.noreply.github.com>
2020-09-16 00:23:37 +0530

    Optimized

--- ETL_Tracker.py
+++ ETL_Tracker.py
@@ -4,31 +4,27 @@ import pandas as pd
 import glob
 import re
 
+def searchword(e,d):
+     if re.search(str(d), e.lower()):
+         if(str(d)=='8807'):
+             print(str(d)+":"+str(e))
+         return True
+     return False
+def searchword1(e,d):
+     d=d+'$'
+     if re.search(d, e.lower()):
+         return True
+     return False
+
 myFiles = glob.glob('*.xlsx')
-print(myFiles)
 for file in myFiles:
     print(file)
     df=pd.read_excel(file,header=0,dtype=object)
-day=int(input("Nunmber of days to be considered:"))
+day=int(input("Number of days to be considered:"))
 y = (dt.date.today() - dt.timedelta(days=day))
 print(y)
 y = y.strftime('%m/%d/%Y %H:%M %p')
 print(y)
-
-def isWordPresent(sentence, word):
-    sentence = re.sub('[^a-zA-Z0-9]',' ',sentence)    
-    s = sentence.split(" ")  
-    for i in s: 
-        if (i.strip() == str(word).strip()):
-            return True
-    return False
-def isWordPresent1(sentence, word):
-    sentence = re.sub('[^a-zA-Z0-9#]',' ',sentence)    
-    s = sentence.split("#")  
-    for i in s: 
-        if (i.strip() == str(word).strip()):
-            return True
-    return False
 
 outlook = win32com.client.Dispatch('Outlook.Application').GetNamespace('MAPI')
 namespace = outlook.Session
@@ -38,31 +34,32 @@ messages = inbox.Items
 messages = messages.Restrict("[ReceivedTime] >= '" + y +"'")
 email_subject = []
 
-i=0
 for x in messages:
-    i=i+1
     sub = x.Subject
-#    if isWordPresent(sub,'Task'):
-    email_subject.append(sub)
+    PCOOO='Production Cutover Green-Light Task UID'
+    if PCOOO in sub:
+        email_subject.append(sub)
+        
 df['#start']=None
 df['#issue']=None
 df['#done']=None
-i=0
+df['There']=None
+
 for d in df['UID']:
-    
+    print(d)
     for e in email_subject:
-        s=isWordPresent(e,d)
+        s=searchword(e,d) 
 #        print(str(d)+":"+str(e)+":"+str(s)+":"+str(start)+":"+str(issue)+":"+str(done))
-        df['There']=s
+        df.loc[df['UID']==d,'There']=s          
         if s is True:
-            done=isWordPresent1(e.lower(),'done')
-            issue=isWordPresent1(e.lower(),'issue')
-            start=isWordPresent1(e.lower(),'start')
-            df['#start'][i]=start
-            df['#issue'][i]=issue
-            df['#done'][i]=done
-    i=i+1
-df['STATUS']=df.apply(lambda x :  '#done' if (x['#done']) else('#issue' if x['#issue'] else('#start' if x['#start'] else 'Task Received')) ,axis=1)
+            done=searchword1(e.lower(),'#done')#re.search('#done$', e.lower())
+            issue=searchword1(e.lower(),'#issue')
+            start=searchword1(e.lower(),'#start')
+            df.loc[df['UID']==d,'#start']=start
+            df.loc[df['UID']==d,'#issue']=issue
+            df.loc[df['UID']==d,'#done']=done
+
+df['Tag Status']=df.apply(lambda x :  '#done' if (x['#done']) else('#issue' if x['#issue'] else('#start' if x['#start'] else ('Received' if x['There'] else 'Yet to receive'))) ,axis=1)
 #df=df.iloc[:,1:]
 df.to_excel(file, sheet_name='ETL_Tracker', engine='xlsxwriter',index=False)
 
